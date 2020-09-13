c63b57da6096663808016d96faed9c4660dd4b75
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-09-13 14:09:11 +0530

    Code

--- Code.py
+++ Code.py
@@ -1,4 +1,5 @@
 import pyttsx3                                     #pip install pyttsx3 and pip install pywin32
+from googletrans import *                          #pip install googletrans
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)          #set male or female voice according to need - to change to female version edit-- voice[1].id
@@ -8,4 +9,10 @@ def speak(str):                                    #can be change to audio file 
     engine.say(str)
     engine.runAndWait()                            #define timestamp
 if __name__ == "__main__":
-    speak(" ")                                     #enter text here
+    a = input("Enter text here: \n")                   #enter text here
+    t = Translator()
+    k = t.translate(a, dest='language name')       #choose preffered language
+    translated = str(k.text)
+    speak(translated)
+    
+  
